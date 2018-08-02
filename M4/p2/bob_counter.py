3fbd18b59129e262696464436b3a9e20fc9e0acf
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-02 18:39:59 +0530

    " -> 0 of 5 passed. pylint: 5.56/10 "

---
+++ M4/p2/bob_counter.py
@@ -0,0 +1,10 @@
+"""string"""
+"""Assume s is a string of lower case characters."""
+def main():
+    str_ = input()
+    coun_ = 0
+    while str_ is "bob":
+        coun_ = coun_+ 1
+    print(coun_)  
+if __name__ == "__main__":
+    main()
