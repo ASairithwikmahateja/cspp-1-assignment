c0b99892354026d1721f2085f9ade4b865186d18
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-02 19:11:03 +0530

    " -> 5 of 5 passed. pylint: 3.33/10 "

--- M4/p2/bob_counter.py
+++ M4/p2/bob_counter.py
@@ -3,8 +3,8 @@
 def main():
     str_ = input()
     coun_ = 0
-    for i in len(str_):
-        if "bob" in str_:
+    for i in range (0,len(str_)-1,1):
+        if str_[i] =='b' and str_[i+1] == 'o' and str_[i+2] == 'b':
             coun_ = coun_+ 1
     print(coun_)  
 if __name__ == "__main__":
