98b7c4357aca6ea9cf13e8be5b1156ba4638fed4
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-02 19:13:12 +0530

    " -> 5 of 5 passed. pylint: 8.89/10 "

--- M4/p2/bob_counter.py
+++ M4/p2/bob_counter.py
@@ -1,10 +1,11 @@
 """string"""
 #Assume s is a string of lower case characters.
 def main():
+    '''Program starts here'''
     str_ = input()
     coun_ = 0
-    for i in range (0,len(str_)-1,1):
-        if str_[i] =='b' and str_[i+1] == 'o' and str_[i+2] == 'b':
+    for i in range(0, len(str_)-1, 1):
+        if str_[i] == 'b' and str_[i+1] == 'o' and str_[i+2] == 'b':
             coun_ = coun_+ 1
     print(coun_)  
 if __name__ == "__main__":
