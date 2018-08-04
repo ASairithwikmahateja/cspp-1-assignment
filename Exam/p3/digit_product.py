3df782b22b57e4a8403146f21c8e9ccac0e63fe0
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-04 11:46:33 +0530

    " -> 0 of 7 passed. pylint: 6.00/10 "

--- Exam/p3/digit_product.py
+++ Exam/p3/digit_product.py
@@ -9,7 +9,6 @@ def main():
     Read any number from the input, store it in variable int_input.
     '''
     int_input = int(input())
-    prod = 0
     while int_input > 1:
     	prod = 1
     	temp = int_input%10
