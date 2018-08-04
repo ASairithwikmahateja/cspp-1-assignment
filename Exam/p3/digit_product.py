538bb98228c65c5bbd94ee0a5d0e9c2f9621128c
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-04 12:30:11 +0530

    " -> 4 of 7 passed. pylint: 10.00/10 "

--- Exam/p3/digit_product.py
+++ Exam/p3/digit_product.py
@@ -11,7 +11,7 @@ def main():
     int_input = int(input())
     prod = 1
     while int_input == "0":
-    	print("0")
+        print("0")
     while int_input > 1:
         temp = int_input%10
         prod = prod*temp
