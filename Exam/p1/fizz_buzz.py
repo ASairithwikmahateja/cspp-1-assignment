4e776d90a1ed44616e6269cadd89e0975e5aa08f
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-04 10:44:47 +0530

    " -> 2 of 7 passed. pylint: 10.00/10 "

--- Exam/p1/fizz_buzz.py
+++ Exam/p1/fizz_buzz.py
@@ -15,9 +15,9 @@ def main():
         else:
             if i%3 == 0 and i%5 == 0:
                 print("Fizz \nBuzz")
-            if i%3 == 0:
+            elif i%3 == 0:
                 print("Fizz")
-            if i%5 == 0:
+            elif i%5 == 0:
                 print("Buzz")
 if __name__ == "__main__":
     main()
