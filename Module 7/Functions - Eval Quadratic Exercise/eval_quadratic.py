2905541003505ad17c46fe0c435e60e810db89fa
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-07 00:31:53 +0530

    new

--- Module 7/Functions - Eval Quadratic Exercise/eval_quadratic.py
+++ Module 7/Functions - Eval Quadratic Exercise/eval_quadratic.py
@@ -5,22 +5,22 @@
 # This function takes in four numbers and returns a single number.
 
 
-def evalQuadratic(a_i, b_i, c_i, x_i):
-    y_i = a_i*x_i*x_i+b_i*x_i+c_i
-    return y_i
+def evalQuadratic(a, b, c, x):
+    return (a*x**2)+b*x+c
+
 def main():
 	data = input()
 	data = data.split(' ')
 	data = list(map(float, data))
 	# print(data)
-	for x_i in range(len(data)):
-		temp = str(data[x_i]).split('.')
+	for x in range(len(data)):
+		temp = str(data[x]).split('.')
 		if(temp[1] == '0'):
-			data[x_i] = int(float(str(data[x_i])))
+			data[x] = int(float(str(data[x])))
 		else:
-			data[x_i] = data[x_i]
+			data[x] = data[x]
 	print(evalQuadratic(data[0],data[1],data[2],data[3]))
 
-if __name__ == "__main__":
+if __name__== "__main__":
 	main()
 
