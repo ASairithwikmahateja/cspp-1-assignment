df65d97a366ee75499e3db50b443ac611f34ca8d
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-07 14:41:16 +0530

    " -> 9 of 10 passed. pylint: 10.00/10 "

--- Module 8/GCD using Iteration/gcd_iter.py
+++ Module 8/GCD using Iteration/gcd_iter.py
@@ -1,15 +1,16 @@
 """Write a Python function, gcdIter(a, b), that takes in two numbers and returns the GCD(a,b) of given a and b.This function takes in two numbers and returns one number."""
-def gcdIter(a, b):
+def gcditer(a, b):
     '''a, b: positive integers
     returns: a positive integer, the greatest common divisor of a & b.'''
-        
-
-
+    for i in range(1, a+1):
+        if a%i == 0 and b%i == 0:
+        	gcd = i
+    return gcd 
 
 def main():
     data = input()
     data = data.split()
-    print(gcdIter(int(data[0]),int(data[1]))) 
+    print(gcditer(int(data[0]), int(data[1]))) 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
--- Module 8/p2/assignment2.py
+++ Module 8/p2/assignment2.py
@@ -1,17 +1,16 @@
 """This function takes in one number and returns one number."""
-def sumofdigits(n):
+def sumofdigit(n_i):
     '''n is positive Integer
     returns: a positive integer, the sum of digits of n.
     '''
-    while n > 0:
-    	s += n%10
-    	return sumofdigits(n/10)
-    print(s)
+    if n_i == 0:
+        return 0
+    return n_i%10+sumofdigit(n_i//10)
 
 def main():
-    a = input()
-    print(sumofdigits(int(a)))  
+    """Program starts here"""
+    a_inpu = input()
+    print(sumofdigit(int(a_inpu)))
 
 if __name__ == "__main__":
     main()
-
