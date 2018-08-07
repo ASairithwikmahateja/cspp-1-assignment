f3bb2a84fb485542a878b2ac86b19a93fb30deb9
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-07 17:38:00 +0530

    new

--- Module 8/GCD using Recursion/gcd_recr.py
+++ Module 8/GCD using Recursion/gcd_recr.py
@@ -2,7 +2,13 @@
 def gcdrecur(a, b):
     '''a, b: positive integers
     returns: a positive integer, the greatest common divisor of a & b.'''
-    
+    if a%b == 0:
+    	return b
+    else:
+    	rmn = a%b
+    	a = b
+    	b = rmn
+    	return gcdrecur(a, b)
 
 def main():
     data = input()
