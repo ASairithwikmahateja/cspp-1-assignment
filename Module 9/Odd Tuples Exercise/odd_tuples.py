e88ad4a7dfd685e74f2b2361e9e31e854afacd74
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-08 21:56:39 +0530

    new

--- Module 9/Odd Tuples Exercise/odd_tuples.py
+++ Module 9/Odd Tuples Exercise/odd_tuples.py
@@ -1,17 +1,21 @@
-'''Program on python'''
-def oddtuples(a_tup):
-    '''aTup: a tuple returns: tuple, every other element of aTup.'''
-    for i in range(0, len(a_tup), 2):
-        print(a_tup[i])
-
+'''Exercise : Odd Tuples'''
+def odd_tuples(a_tup):
+    '''
+    aTup: a tuple
+    returns: tuple, every other element of aTup.
+    '''
+    l_1 = len(a_tup)
+    res_1 = ()
+    for i in range(0, l_1, 2):
+        res_1 += (a_tup[i],)
+    return res_1
 def main():
-	#Main program
+    '''Main Function.'''
     data = input()
-    data = data.split(",")
+    data = data.split()
     a_tup = ()
     for j in range(len(data)):
-        a_tup += (int(data[j]),)
-    print(oddtuples(a_tup))
-
+        a_tup += (data[j],)
+    print(odd_tuples(a_tup))
 if __name__ == "__main__":
     main()
