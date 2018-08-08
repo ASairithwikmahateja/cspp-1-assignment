a991ced4424051cfabdd98459c6923544d94bb0b
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-08 22:14:26 +0530

    " -> 9 of 9 passed. pylint: 10.00/10 "

--- Module 9/p1/assignment1.py
+++ Module 9/p1/assignment1.py
@@ -1,14 +1,33 @@
-'''Program Starts here'''
-def is_word_guessed(secret_word, letters_guessed):
-    '''Function call'''
-    for i in secret_word:
-        if letters_guessed in i:
-            return True
-        else:
-            return False
+'''
+Exercise: Assignment-1
+First, implement the function isWordGuessed that takes in two parameters -
+a string, secret_word, and a list of letters, letters_guessed. This function
+returns a boolean - True if secret_word has been guessed (ie, all the letters of
+secret_word are in letters_guessed) and False otherwise.
+'''
 
+
+def is_word_guessed(secret_word, letters_guessed):
+    '''
+    secret_word: string, the word the user is guessing
+    letters_guessed: list, what letters have been guessed so far
+    returns: boolean, True if all the letters of secret_word are in letters_guessed;
+      False otherwise
+    '''
+    l_1 = len(secret_word)
+    l_2 = len(letters_guessed)
+    cnt_1 = 0
+    for i in range(l_1):
+        for j in range(l_2):
+            if secret_word[i] == letters_guessed[j]:
+                cnt_1 += 1
+    if cnt_1 >= l_1:
+        return True
+    return False
 def main():
-    '''Main function for the program'''
+    '''
+    Main function for the program
+    '''
     user_input = input()
     if user_input:
         data = user_input.split()
@@ -16,10 +35,10 @@ def main():
     else:
         data = []
         secret_word = ""
-    list1 = []
+    list_1 = []
     for j in range(1, len(data)):
-        list1.append(data[j][0])
-    print(is_word_guessed(secret_word, list1))
+        list_1.append(data[j][0])
+    print(is_word_guessed(secret_word, list_1))
 
 if __name__ == "__main__":
     main()
