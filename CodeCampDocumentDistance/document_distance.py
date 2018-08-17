8fcbc71dd4ab57197c9d672a5872abc93616ce26
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-17 18:14:34 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- CodeCampDocumentDistance/document_distance.py
+++ CodeCampDocumentDistance/document_distance.py
@@ -1,20 +1,38 @@
 '''
     Document Distance - A detailed description is given in the PDF
 '''
+import math
 
 def similarity(dict1, dict2):
     '''
         Compute the document distance as given in the PDF
     '''
+    dict1.lower()
+    dict2.lower()
+    dictionary = {}
     word_freq = {}
-    dic = dict1.split(" ")
-    print(dic)
-    for word in dic:
-    	key_ =  word
-    	print(key_)
-    	value_ = list(dic).count(word)
-    	print(value_)
-    word_freq = dict(zip(key_, value_))
+    list_1 = dict1.split(" ") + dict2.split(" ")
+    spl_char = "!@#$%^&*()-_+"
+    for i in list_1:
+        if i in spl_char:
+            list_1.remove(i)
+    for i in list_1:
+        dictionary[i] = list_1.count(i)
+    dict_3 = load_stopwords("stopwords.txt")
+    for i in dict_3:
+        if i in dictionary:
+            del dictionary[i]
+    for i in list_3:
+        word_freq[i] = [dict1.split(" ").count(i), dict2.split(" ").count(i)]
+    numer_n = 0
+    denom_n1 = 0
+    denom_n2 = 0
+    for i in word_freq:
+        numer_n = sum(word_freq[i][0]*word_freq[i][1])
+        denom_n1 = sum(word_freq[i][0]**2)
+        denom_n2 = sum(word_freq[i][1]**2)
+    denom_n = math.sqrt(denom_n1)*math.sqrt(denom_n2)
+    return numer_n/denom_n
 
 def load_stopwords(filename):
     '''
