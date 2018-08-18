345a4f05bd4699c7e479c9e47de97ac182a2b4d2
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-18 13:12:40 +0530

    " -> 0 of 1 passed. pylint: 7.00/10 "

--- Module 18/p2/search.py
+++ Module 18/p2/search.py
@@ -37,7 +37,7 @@ def search(search_index, query):
         collect all the values for the words that are in the search_index
         make a set of doc_id and return
     '''
-    dictionary_keys = []
+    dictionary_keys = {}
     query = query.lower()
     if query(int([search_index])) in dictionary_keys:
         dictionary_keys = dictionary_keys.append(query(int([search_index])))
