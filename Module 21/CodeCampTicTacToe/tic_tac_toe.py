fe7a2447fddc3011ebab53fa11e0b2a33837b3ce
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-24 14:32:06 +0530

    " -> 7 of 14 passed. pylint: -3.00/10 "

--- Module 21/CodeCampTicTacToe/tic_tac_toe.py
+++ Module 21/CodeCampTicTacToe/tic_tac_toe.py
@@ -8,14 +8,17 @@ def tic_tac_toe(mat):
 		for k in range(3):
 			l = [i for i in mat[j][k]]
 			lst.append(l)
-	for itera in range(len(lst)-6):
-		if lst[itera] == lst[itera+3] and lst[itera+3] == lst[itera+6]:
-			if ''.join(lst[itera]) in ('x','o'):
-				print(''.join(lst[itera]))
-			else:
-				print('invalid input')
-		elif lst[itera+2] == lst[itera+4] and lst[itera+4] == lst[itera+6]:
-			print(lst[itera])
+	if lst.count(0) < 6:
+		for itera in range(len(lst)-6):
+			if lst[itera] == lst[itera+3] and lst[itera+3] == lst[itera+6]:
+				if ''.join(lst[itera]) in ('x','o'):
+					print(''.join(lst[itera]))
+				else:
+					print('invalid input')
+	else:
+		print('invalid game')
+	'''elif lst[itera+2] == lst[itera+4] and lst[itera+4] == lst[itera+6]:
+		print(lst[itera])'''
 	'''elif lst[itera] == lst[itera+4]:
 		print(lst[itera])'''
 
