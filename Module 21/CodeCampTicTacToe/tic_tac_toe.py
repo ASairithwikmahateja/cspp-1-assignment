db64a83388541f950eaab2ef4aaeb9d3b0700184
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-24 13:52:52 +0530

    " -> 6 of 14 passed. pylint: -2.50/10 "

--- Module 21/CodeCampTicTacToe/tic_tac_toe.py
+++ Module 21/CodeCampTicTacToe/tic_tac_toe.py
@@ -8,7 +8,7 @@ def tic_tac_toe(mat):
 		for k in range(3):
 			l = [i for i in mat[j][k]]
 			lst.append(l)
-	if lst.count('x') > 6 or lst.count('o') > 6:
+	if lst.count('x') > 5 or lst.count('o') > 5:
 		print('invalid game')
 	else:
 		for itera in range(len(lst)-6):
