9d12ceb9ccdb15760a9dcf7ec21f53c48cac9e52
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-24 11:42:57 +0530

    " -> 0 of 14 passed. pylint: -2.14/10 "

---
+++ Module 21/CodeCampTicTacToe/tic_tac_toe.py
@@ -0,0 +1,34 @@
+def tic_tac_toe(mat):
+	for row in mat:
+		if row.count(row[0]) == 3:
+			print(row[0])
+			return row[0]
+	lst = []
+	for j in range(3):
+		for k in range(3):
+			l = [i for i in mat[j][k]]
+			lst.append(l)
+	for itera in range(len(lst)-6):
+		if lst[itera] == lst[itera+3] and lst[itera+3] == lst[itera+6]:
+			if lst[itera] == '.':
+				return None
+			print(lst[itera])
+		'''elif lst[itera] == lst[itera+4] and lst[itera+4] == lst[itera+7]:
+			print(lst[itera])
+		elif lst[itera+2] == lst[itera+4] and lst[itera+4] == lst[itera+6]:
+			print(lst[itera])'''
+
+def read_matrix():
+	 matrix = []
+	 for i in range(3):
+	 	l =[i for i in input().split(" ")]
+	 	matrix.append(l)
+	 print(matrix)
+	 return matrix
+
+def main():
+	mat = read_matrix()
+	return tic_tac_toe(mat)
+
+if __name__ == "__main__":
+	main()
