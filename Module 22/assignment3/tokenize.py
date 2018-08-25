9fc30debc9e3c0bc182ee3cea4882de3b696ae7f
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-25 11:28:14 +0530

    " -> 0 of 4 passed. pylint: 3.57/10 "

--- Module 22/assignment3/tokenize.py
+++ Module 22/assignment3/tokenize.py
@@ -15,19 +15,6 @@ def tokenize(line):
     		dictfreq[i] += 1
     print(dict(zip(dictfreq.keys(),dictfreq.values())))
 
-def clean_string(string):
-    '''
-    String Cleaning method
-    '''
-    spc = "!@#$%^&*() .,"
-    str_new = ""
-    for i in string:
-        if i in spc:
-            str_new += ""
-        else:
-            str_new += i
-    return str_new
-
 def main():
     '''
     Main Function
@@ -36,11 +23,6 @@ def main():
     lst = []
     for i in range(n_l):
         line = input().split(" ")
-        print(line)
-        lst.append(line)
-        line = clean_string(lst)
-    print(lst)
-    tokenize(line)
 
 if __name__ == '__main__':
     main()
