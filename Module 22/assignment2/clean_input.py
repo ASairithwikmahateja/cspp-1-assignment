ad37bc0d1d342ef8b565b56c960fc19a2e66fab3
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-25 10:51:39 +0530

    " -> 0 of 3 passed. pylint: 4.62/10 "

--- Module 22/assignment2/clean_input.py
+++ Module 22/assignment2/clean_input.py
@@ -5,13 +5,14 @@ alphabets in both upper and lower case and numbers.
 
 def clean_string(string):
     spc = "!@#$%^&*()"
+    str_new = ""
     for i in string:
     	if i in spc:
     		str_new += ""
     	else:
     		str_new += i
     print(str_new)
-    
+
 def main():
     string = input()
     print(clean_string(string))
