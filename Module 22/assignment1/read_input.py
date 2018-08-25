9e87d51ddf30745eb69e59455ba8665d277e33ad
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-25 10:40:01 +0530

    " -> 2 of 2 passed. pylint: 5.00/10 "

--- Module 22/assignment1/read_input.py
+++ Module 22/assignment1/read_input.py
@@ -8,8 +8,8 @@ def main():
     for i in range(n):
     	line = input()
     	lst.append(line)
-    	lst.split(",")
-    print(lst)
+    for i in lst:
+    	print(i)
 
 if __name__ == '__main__':
     main()
