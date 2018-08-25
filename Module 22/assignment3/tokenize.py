1178df9d3dac24067d48272d78ea6e44073063d9
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-08-25 11:14:50 +0530

    " -> 0 of 4 passed. pylint: 5.00/10 "

--- Module 22/assignment3/tokenize.py
+++ Module 22/assignment3/tokenize.py
@@ -3,11 +3,26 @@ Write a function to tokenize a given string and return a dictionary with the fre
 each word
 '''
 
-def tokenize(string):
-    pass
-            
+def tokenize(line):
+    '''
+    Tokenize Function 
+    '''
+    dictfreq = {}
+    for i in line:
+    	if i not in dictfreq.keys():
+    		dictfreq[i] = 0
+    	else:
+    		dictfreq[i] += 1
+    print(dict(zip(dictfreq.keys(),dictfreq.values())))
+
 def main():
-    pass
+    '''
+    Main Function
+    '''
+    n_l = int(input())
+    for i in range(n_l):
+        line = input().split(" ")
+    tokenize(line)
 
 if __name__ == '__main__':
     main()
